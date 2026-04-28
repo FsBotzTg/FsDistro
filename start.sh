5f1b0af03b6d31135cbcc8713e8789a400c36559
Firos Sha Muhammad <fsbotztg@gmail.com>
2026-04-28 22:52:32 +0530

    Add start.sh script for Termux environment setup

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+unset LD_PRELOAD
+proot --link2symlink -0 -r ~/ubuntu \
+-b /dev \                                                                        -b /proc \
+-b /sys \                                                                        -w /root \
+/usr/bin/env -i \
+HOME=/root \
+TERM=$TERM \                                                                     LANG=C.UTF-8 \
+/bin/sh
